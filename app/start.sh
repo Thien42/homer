e2e56f82381099d5c4256a79495f193f37d2be1f
Thien An Lacape <thienan.lacape@epitech.eu>
2017-01-03 13:28:56 +0100

    Update

--- app/start.sh
+++ app/start.sh
@@ -10,12 +10,12 @@ bundle install
 bundle exec rake db:create
 bundle exec rake db:migrate
 
-if [ -n "$RAILS_ENV" ]
-then
-    if [ "$RAILS_ENV" == "production" ]
-    then
-	bundle exec rake assets:precompile
-    fi
-fi
+# if [ -n "$RAILS_ENV" ]
+# then
+#    if [ "$RAILS_ENV" == "production" ]
+#    then
+#	bundle exec rake assets:precompile
+#    fi
+# fi
 
 rails server -b0.0.0.0
