2b6e37eab30368c29bafb9fd290301a1840c1208
Thien An Lacape <thienan.lacape@epitech.eu>
2017-02-06 11:20:23 +0100

    Fix session test

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -4,9 +4,6 @@ class ApplicationController < ActionController::Base
   helper_method :user_logged_in, :current_user
 
   def current_user
-
-    session[:user_email] = "florian.vuillemot@epitech.eu"
-    session[:azure_token] = "test"
     current_user = User.find_by_email(session[:user_email])
     if current_user.nil?
       current_user = User.new
